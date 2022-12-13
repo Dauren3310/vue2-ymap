69418297ed81d51ebe2aff60e537315f2b410ec8
Dauren Babayev <daur3310@gmail.com>
2022-12-14 01:44:42 +0600

    feat: enabled YMap

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<!-- <template>
+  <div id="app">
+    <h1>Choco</h1>
+    <YMap />
+  </div>
+</template>
+
+<script>
+import YMap from "./components/YMap.vue";
+
+export default {
+  name: "app",
+  components: {YMap}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+<template>
+  <div id="app">
+    <yandex-map :coords="coords" @click="onClick">
+      <ymap-marker 
+        marker-id="123" 
+        :coords="coords"
+        :balloon-template="balloonTemplate"
+      ></ymap-marker>
+    </yandex-map>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    coords: [54, 39],
+  }),
+  computed: {
+    balloonTemplate() {
+      return `
+        <h1 class="red">Hi, everyone!</h1>
+        <p>I am here: ${this.coords}</p>
+        <img src="http://via.placeholder.com/350x150">
+      `
+    }
+  },
+  methods: {
+    onClick(e) {
+      this.coords = e.get('coords');
+    }
+  }  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.ymap-container {
+  height: 600px;
+}
+.red {
+  color: red;
+}
+</style>
---
+++ src/components/YMap.vue
@@ -0,0 +1,44 @@
+<script>
+import { yandexMap, ymapMarker } from 'vue-yandex-maps'
+
+export default {
+  data: () => ({
+    coords: [
+      54.82896654088406,
+      39.831893822753904,
+    ],
+  }),
+  methods: {
+    onClick(e) {
+      this.coords = e.get('coords');
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="yandex-map-container">
+  <!-- <div> -->
+    <!-- <h1>Ymap</h1> -->
+    <yandex-map 
+    style="width: 100%; height: 300px" v-if="true"
+    :coords="coords"
+    :zoom="10" 
+    @click="onClick"
+  >
+    <ymap-marker 
+      :coords="coords" 
+      marker-id="123" 
+      hint-content="some hint" 
+    />
+  </yandex-map>
+</div>
+
+</template>
+
+<style scoped>
+  .yandex-map-container {
+    height: 1000px;
+    width: 1000px;
+  }
+</style>
